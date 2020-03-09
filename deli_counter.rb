e5786a131fd72120dd01be4c696288ff79bf98de
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 01:51:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+
+#$katz_deli = ["Andy","Bill","Charles","David","Emily"]
+$katz_deli = []
+
+def line(deliline)
+  a = deliline.size 
+  if a == 0 then
+    puts "The line is currently empty."
+  else
+  delistring = "The line is currently: "
+  piece = " "
+  b = 1
+  c = 0
+  a.times do
+    piece = deliline[c]
+    delistring = delistring + "#{b}. #{piece} "
+    b += 1 
+    c += 1 
+  end
+  end
+  puts delistring
+end
+
+def take_a_number(deliline, name)
+  d = deliline.size + 1 
+  puts "Welcome, #{name}. You are number #{d} in line."
+  $katz_deli.push(name)
+end
+
+def now_serving(deliline)
+  if deliline.size == 0 then
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deliline[0]}"
+    $katz_deli.shift
+  end
+end
+
+
+take_a_number($katz_deli,"Ada")
+take_a_number($katz_deli,"Grace")
+take_a_number($katz_deli,"Kent")
+line($katz_deli)
+now_serving($katz_deli)
+line($katz_deli)
+take_a_number($katz_deli,"Matz")
+line($katz_deli)
+now_serving($katz_deli)
+line($katz_deli)
+
